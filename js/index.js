d47e61d3b2e840896439e86e86851471bf3f2cc3
Yousif <yossifmazen@gmail.com>
2023-11-16 17:12:45 +0300

    contact us

---
+++ js/index.js
@@ -0,0 +1,23 @@
+var slider = tns({
+    container: '.brands-slider',
+    items: 5,
+    slideBy: 1,
+    autoplay: true,
+    autoplayTimeout: 3500,
+    autoplayButtonOutput: false,
+    controls: false,
+    navPosition: 'bottom',
+    mouseDrag: true,
+    // responsive: {
+    //     640: {
+
+    //         items: 2
+    //     },
+    //     700: {
+    //         gutter: 30
+    //     },
+    //     900: {
+    //         items: 3
+    //     }
+    // }
+}); 
